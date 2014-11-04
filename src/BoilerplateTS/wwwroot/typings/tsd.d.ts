f837cb095695fbbdea8fe7aa731a762aa9537171
goldnarms <arnstej@gmail.com>
2014-11-04 15:41:35 +0100

    Added Angular, Bootstrap, TypeScript, Karma +++

---
+++ src/BoilerplateTS/wwwroot/typings/tsd.d.ts
@@ -0,0 +1,5 @@
+
+/// <reference path="angularjs/angular.d.ts" />
+/// <reference path="jquery/jquery.d.ts" />
+/// <reference path="underscore/underscore.d.ts" />
+/// <reference path="bootstrap/bootstrap.d.ts" />
